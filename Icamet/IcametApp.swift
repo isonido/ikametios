22819f1a13586b6c0ab3ee2fe0cf6b6aca0e4ae2
Andrey Androsov <andrey.sonido@gmail.com>
2022-11-27 18:07:43 +0300

    Initial Commit

---
+++ Icamet/IcametApp.swift
@@ -0,0 +1,17 @@
+//
+//  IcametApp.swift
+//  Icamet
+//
+//  Created by MacBook on 27.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct IcametApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
